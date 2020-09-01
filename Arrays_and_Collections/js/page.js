04824ca1d8a170696405aa5a55b3072f2496f602
LindsayPeterson <lindsay.peterson@legitscript.com>
2020-09-01 16:04:52 -0700

    create sets

--- Arrays_and_Collections/js/page.js
+++ Arrays_and_Collections/js/page.js
@@ -1,58 +1,74 @@
-// Accessing the objects
+// Accessing the objects// Accessing the objects
 let ctx = document.getElementById('monthlySales').getContext('2d');
 let pieCtx = document.getElementById('deptSales').getContext('2d');
 
 let yearlyLabel = document.getElementById('yearlyTotal');
 
-// Monthly Totals
+// Values from the form
+let newAmount = document.getElementById('itemAmount');
+let newMonth = document.getElementById('monthId');
+
 let yearlyTotal = 0;
 
-// Code goes here
+const monthlySales = new Set();
+const monthlyLabels = new Set();
+
+function addSale(){
+    monthlySales.add(newAmount.value);
+    monthlyLabels.add(newMonth.value);
+    alert('You have entered in ' + monthlySales.size + ' sales');
+    console.log(monthlySales);
+}
+
+function deleteVal(){
+    monthlySales.delete('1500');
+    console.log(monthlySales);
+}
 
 // Bar chart
-var monthlySalesChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: monthlyLabels,
-        datasets: [{
-            label: '# of Sales',
-            data: monthlySales,
-            backgroundColor: [
-                'rgba(238, 184, 104, 1)',
-                'rgba(75, 166, 223, 1)',
-                'rgba(239, 118, 122, 1)',
-            ],
-            borderWidth: 0
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
+// var monthlySalesChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: monthlyLabels,
+//         datasets: [{
+//             label: '# of Sales',
+//             data: monthlySales,
+//             backgroundColor: [
+//                 'rgba(238, 184, 104, 1)',
+//                 'rgba(75, 166, 223, 1)',
+//                 'rgba(239, 118, 122, 1)',
+//             ],
+//             borderWidth: 0
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero: true
+//                 }
+//             }]
+//         }
+//     }
+// });
 
-// Pie Chart
-var deptSalesChart = new Chart(pieCtx, {
-    type: 'pie',
-    data: {
-        labels: deptLabels,
-        datasets: [{
-            label: '# of Sales',
-            data: deptSales,
-            backgroundColor: [
-                'rgba(238, 184, 104, 1)',
-                'rgba(75, 166, 223, 1)',
-                'rgba(239, 118, 122, 1)',
-            ],
-            borderWidth: 0
-        }]
-    },
-    options: {
+// // Pie Chart
+// var deptSalesChart = new Chart(pieCtx, {
+//     type: 'pie',
+//     data: {
+//         labels: deptLabels,
+//         datasets: [{
+//             label: '# of Sales',
+//             data: deptSales,
+//             backgroundColor: [
+//                 'rgba(238, 184, 104, 1)',
+//                 'rgba(75, 166, 223, 1)',
+//                 'rgba(239, 118, 122, 1)',
+//             ],
+//             borderWidth: 0
+//         }]
+//     },
+//     options: {
         
-    }
-})
+//     }
+// })
