8907980ec914dbe979cc7d53720e9cb0556cb459
LindsayPeterson <lindsay.peterson@legitscript.com>
2020-09-17 14:12:39 -0700

    intro to maps

--- Arrays_and_Collections/js/page.js
+++ Arrays_and_Collections/js/page.js
@@ -1,103 +1,60 @@
-// Accessing the objects// Accessing the objects
-let ctx = document.getElementById('monthlySales').getContext('2d');
-let pieCtx = document.getElementById('deptSales').getContext('2d');
-
-let yearlyLabel = document.getElementById('yearlyTotal');
-
-// Values from the form
-let newAmount = document.getElementById('itemAmount');
-let newMonth = document.getElementById('monthId');
-
+// Accessing the objects
+var ctx = document.getElementById('monthlySales').getContext('2d');
+var pieCtx = document.getElementById('deptSales').getContext('2d');
+var yearlyLabel = document.getElementById('yearlyTotal');
+var newAmount = document.getElementById('itemAmount');
+var newMonth = document.getElementById('monthId');
 let hikingRadio = document.getElementById('hiking');
 let runningRadio = document.getElementById('running');
 let huntingRadio = document.getElementById('hunting');
 
-let yearlyTotal = 0;
+// Monthly Totals
+var yearlyTotal = 0;
 
-const monthlySales = new Set();
-const monthlyLabels = new Set();
+const monthlySales = new Map();
 
-const categories = new WeakSet();
+// Add Sales
+function addSale(){
+    monthlySales.set(newMonth.value, newAmount.value)
+    console.log(monthlySales)
+}
 
-let hiking = {category: 'Hiking'};
-let running = {category: 'Running'};
-let hunting = {category: 'Hunting'};
-
-function addSale() {
-    monthlySales.add(parseInt(newAmount.value));
-    monthlyLabels.add(newMonth.value);
-
-    yearlyTotal = 0;
-
-    monthlySalesChart.data.datasets.forEach((dataset) => {
-        dataset.data = [];
-    })
-
-    for (let amount of monthlySales) {
-        yearlyTotal = amount + yearlyTotal;
-        yearlyLabel.innerHTML = yearlyTotal;
-
-        monthlySalesChart.data.datasets.forEach((dataset) => {
-            dataset.data.push(amount);
-        })
-
-    }
-
-    monthlySalesChart.data.labels = Array.from(monthlyLabels);
-
-    monthlySalesChart.update();
-
-    if (hikingRadio.checked) {
-        categories.add(hiking);
-    } else if (runningRadio.checked) {
-        categories.add(running);
-    } else if (huntingRadio.checked) {
-        categories.add(hunting);
-    } else {
-        // do something else
-    }
-
-    console.log(categories);
+function findSale(){
 
 }
 
-function deleteVal(){
-    monthlySales.forEach((value1, value2, monthlySales) => {
-        alert(value1);
-    })
-}
+function fillValue() {
 
-function addTotal(){
 }
 
 // Bar chart
-var monthlySalesChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: [],
-        datasets: [{
-            label: '# of Sales',
-            data: monthlySales,
-            backgroundColor: [
-                'rgba(238, 184, 104, 1)',
-                'rgba(75, 166, 223, 1)',
-                'rgba(239, 118, 122, 1)',
-            ],
-            borderWidth: 0
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
+// var monthlySalesChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: [],
+//         datasets: [{
+//             label: '# of Sales',
+//             data: [],
+//             backgroundColor: [
+//                 'rgba(238, 184, 104, 1)',
+//                 'rgba(75, 166, 223, 1)',
+//                 'rgba(239, 118, 122, 1)',
+//             ],
+//             borderWidth: 0
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero: true
+//                 }
+//             }]
+//         }
+//     }
+// });
 
-// // Pie Chart
+// Pie Chart
 // var deptSalesChart = new Chart(pieCtx, {
 //     type: 'pie',
 //     data: {
@@ -114,5 +71,6 @@ var monthlySalesChart = new Chart(ctx, {
 //         }]
 //     },
 //     options: {
+        
 //     }
 // })
