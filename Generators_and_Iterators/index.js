14b8d0cf2de0cdb8566832e5c08f750cc31c073d
LindsayPeterson <lindsay.peterson@legitscript.com>
2020-12-21 16:41:13 -0800

    add db to iterate

--- Generators_and_Iterators/index.js
+++ Generators_and_Iterators/index.js
@@ -1,34 +1,19 @@
 #! /usr/bin/env node
+const readline = require('readline').createInterface({
+    input:  process.stdin,
+    output: process.stdout,
+})
 
-const arr = [0, 3, 4, 6];
-
-const it = arr[Symbol.iterator]();
-console.log(it.next());
-console.log(it.next());
-console.log(it.next());
-console.log(it.next());
-console.log(it.next());
-
-const map = new Map();
-map.set('key1', 'value1');
-map.set('key2', 'value2');
-const mapIterator = map[Symbol.iterator]();
-console.log(mapIterator.next().value);
-console.log(mapIterator.next().value);
-
-for (const [key, value] of map) {
-    console.log(`${key} and ${value}`);
-}
-
-const mySet = new Set();
-mySet.add('one');
-mySet.add('two');
-
-const setIterator = mySet[Symbol.iterator]();
-console.log(setIterator.next());
-console.log(setIterator.next());
-console.log(setIterator.next());
-
-for(const value of mySet) {
-    console.log(value)
-}
+readline.question('What would you like to log today? ', async (item) => {
+    const { data }  = await axios.get('http://localhost:3001/food');
+    const it = data[Symbol.interator]();
+    let position = it.next();
+    while(!position.done) {
+        const food = position.value.name;
+        if (food === item) {
+            console.log(`${item} has  ${position.value.calories} calories`)
+        }
+        position = it.next();
+    }
+    readline.close()
+})
