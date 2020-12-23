1abc06b764cd5a0e04928311dc1b058463dd159e
LindsayPeterson <lindsay.peterson@legitscript.com>
2020-12-22 16:42:43 -0800

    how many calories are in two oreos

--- Generators_and_Iterators/index.js
+++ Generators_and_Iterators/index.js
@@ -6,7 +6,7 @@ const readline = require('readline').createInterface({
     prompt: 'enter command > ',
 });
 readline.prompt();
-readline.on('line', line => {
+readline.on('line', async line => {
     switch (line.trim()) {
         case 'list vegan foods':
             {
@@ -25,9 +25,9 @@ readline.on('line', line => {
                                     const current = veganOnly[idx];
                                     idx++;
                                     if (current) {
-                                        return { value: current, done: false}
+                                        return { value: current, done: false};
                                     } else {
-                                        return { value: current, done: true}
+                                        return { value: current, done: true};
                                     }
                                 },
                             };
@@ -41,20 +41,65 @@ readline.on('line', line => {
             }
             break;
         case 'log' :
-            {
-                readline.question('What would you like to log today? ', async (item) => {
-                    const { data }  = await axios.get('http://localhost:3001/food');
-                    const it = data[Symbol.iterator]();
-                    let position = it.next();
-                    while(!position.done) {
-                        const food = position.value.name;
-                        if (food === item) {
-                            console.log(`${item} has  ${position.value.calories} calories`)
+            const { data }  = await axios.get('http://localhost:3001/food');
+            const it = data[Symbol.iterator]();
+            let actionIt;
+
+            const actionIterator = {
+                [Symbol.iterator]() {
+                    const positions = [...this.actions];
+                    return {
+                        [Symbol.iterator]() {
+                            return this;
+                        },
+                        next(...args) {
+                            if(positions.length > 0) {
+                                const position = positions.shift();
+                                const result = position(...args);
+                                return { value: result, done: false}
+                            } else {
+                                return { done: true };
+                            }
                         }
-                        position = it.next();
-                    }
-                    readline.prompt();
-                });
+                    };
+                },
+                actions: [askForServingSize, displayCalories],
             };
-    };
-})
+            function askForServingSize(food) {
+                readline.question('How many servings did you eat? ',
+                servingSize => {
+                    actionIt.next(servingSize, food)
+                },
+            );
+        }
+
+        function displayCalories(servingSize, food) {
+            const calories = food.calories;
+            console.log(
+                `${
+                food.name
+                } with a serving size of ${servingSize} has ${Number.parseFloat(
+                calories * parseInt(servingSize, 10),
+                ).toFixed()} calories. `,
+                );
+            actionIt.next();
+            readline.prompt();
+        }
+
+        readline.question('What would you like to log today? ', async (item) => {
+            let position = it.next();
+            while(!position.done) {
+                const food = position.value.name;
+                if (food === item) {
+                    console.log(`${item} has  ${position.value.calories} calories`)
+                    actionIt = actionIterator[Symbol.iterator]();
+                    actionIt.next(position.value);
+                }
+                position = it.next();
+            }
+            readline.prompt();
+        });
+        break;
+    }
+    readline.prompt();
+});
